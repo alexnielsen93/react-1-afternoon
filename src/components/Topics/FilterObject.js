54b04c39c87adf85f5795aee6d8fe874ea444f6f
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-04-01 14:32:04 -0600

    first commit

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+export default class FilterObject extends Component{
+  render(){
+    return(
+
+      <p>FilterObject Component</p>
+    )
+
+  }
+
+}
---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+export default class Palindrome extends Component{
+  render(){
+    return(
+
+      <p>Palindrome Component</p>
+    )
+
+  }
+
+}
