0500796d909afffbc6b3074bd5a075666ec22971
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-04-01 16:49:04 -0600

    DOOOOOOOOONNNNNNNNE

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -2,10 +2,63 @@ import React, {Component} from 'react'
 
 
 export default class Sum extends Component{
-  render(){
+  
+    constructor(){
+      super()
+      this.state={
+        number1 : 0,
+        number2 : 0,
+        sum : 0
+
+      }
+      
+    }
+    handleChange(userInput, key){
+      console.log(this.state.number1)
+      console.log(this.state.number2)
+      this.setState({
+        [key]:parseInt(userInput)
+
+      })
+      console.log(this.state.number1)
+      console.log(this.state.number2)
+    }
+
+    sum(){
+      let sum = this.state.sum
+      let num1 = this.state.number1
+      let num2 = this.state.number2
+      sum = num1+num2
+    
+      this.setState({
+        sum : sum
+      }
+      
+      )
+      console.log(this.state.sum)
+
+    }
+
+    render(){
     return(
 
-      <p>Sum Component</p>
+
+       <div className = "puzzleBox sumPB">
+
+        <h4>Sum</h4>
+      
+        <input type = 'number' key = 'number1'className = "inputLine" onChange = {(e) => this.handleChange(e.target.value, "number1")}></input>
+
+        <input type = 'number' key = 'number2' className = 'inputLine' onChange = {(e) => this.handleChange(e.target.value, "number2")}></input>
+
+        <button className = 'confirmationButton' onClick = {()=> this.sum ()}>Add</button>
+        <span className = "resultsBox">Sum : {JSON.stringify(this.state.sum)}</span>
+    
+
+
+      </div>
+
+
     )
 
   }
